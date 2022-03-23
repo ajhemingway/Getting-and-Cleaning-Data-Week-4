c90ab546e2ef38e80e803bc639f63904cf700512
Adam Hemingway <ahemingway@hps.hadleyschools.org>
2022-03-23 14:46:04 -0400

    sorting values into df for mean and std

--- run_analysis.R
+++ run_analysis.R
@@ -6,6 +6,7 @@ ftrsub <- ftr[grep("mean", ftr$V2), ] #extract only values for mean
 ftrsub2 <- ftr[grep("std", ftr$V2), ] #extract only values for std
 mrg2 <- rbind(ftrsub, ftrsub2) #combine features into one df
 mrg2 <- arrange(mrg2, mrg2$V1)
-cols_train <- mrg2[, 1]
+cols_trainTest <- mrg2[, 1] #identify integer values for matching mrg columns
+vals <- mrg[, cols_trainTest] #df with only values for mean and std
 
 #next push
