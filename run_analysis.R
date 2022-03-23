f1c1d0f820d5124662f09d18a44a7e2c8c1a287f
Adam Hemingway <ahemingway@hps.hadleyschools.org>
2022-03-23 11:55:34 -0400

    rearrange top three lines

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,10 @@
+test <- read.table("X_test.txt") #assumes files are all in wd
+train <- read.table("X_train.txt")
+ftr <- read.table("features.txt") 
 mrg <- rbind(test, train)
 ftrsub <- ftr[grep("mean", ftr$V2), ]
 ftrsub2 <- ftr[grep("std", ftr$V2), ]
 mrg2 <- rbind(ftrsub, ftrsub2)
 mrg2 <- arrange(mrg2, mrg2$V1)
+#next
 
