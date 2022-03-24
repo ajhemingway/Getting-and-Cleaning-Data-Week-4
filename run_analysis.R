23ee878507e2ca7c2663668c1ac6544a7ea899c5
Adam Hemingway <ahemingway@hps.hadleyschools.org>
2022-03-24 14:53:01 -0400

    includes transmute for aligning activtity names

--- run_analysis.R
+++ run_analysis.R
@@ -4,14 +4,13 @@ ftr <- read.table("features.txt")
 y_test <- read.table("y_test.txt")
 y_train <- read.table("y_train.txt")
 mrg <- rbind(test, train)
+mrg2 <- data.frame(t(mrg)) #transmute to align with features description
 ftrsub <- ftr[grep("mean", ftr$V2), ] #extract only values for mean
 ftrsub2 <- ftr[grep("std", ftr$V2), ] #extract only values for std
-mrg2 <- rbind(ftrsub, ftrsub2) #combine features into one df
-mrg2 <- arrange(mrg2, mrg2$V1)
+mrg3 <- rbind(ftrsub, ftrsub2) #combine features into one df
+mrg3 <- arrange(mrg3, mrg3$V1)
 cols_trainTest <- mrg2[, 1] #identify integer values for matching mrg columns
 vals <- mrg[, cols_trainTest] #df with only values for mean and std
-y_test <- read.table("y_test.txt") 
-y_train <- read.table("y_train.txt")
 y_new <- rbind(y_test, y_train)
 colnames(y_new) <- "Activity" #assign label to activity types
 namesMatch <- cbind(y_new, vals)
