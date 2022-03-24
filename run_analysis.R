053ec04c4c98d73bffc4992c289efbf30f0ec18d
Adam Hemingway <adam.j.hemingway@gmail.com>
2022-03-24 19:12:37 -0400

    rearrange with column names in place

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,21 @@
 run_analysis <- function(x){
-        test <- read.table("X_test.txt") #assumes files are all in current wd
+        test <- read.table("X_test.txt") #assume files are all in current wd
         train <- read.table("X_train.txt")
         ftr <- read.table("features.txt")
         y_test <- read.table("y_test.txt")
         y_train <- read.table("y_train.txt")
+        subtest <- read.table("subject_test.txt")
+        subtrain <- read.table("subject_train.txt")
         mrg <- rbind(test, train)
-        mrg2 <- data.frame(t(mrg)) #transmute to align with features description
-        ftrsub <- ftr[grep("mean", ftr$V2), ] #extract only values for mean
-        ftrsub2 <- ftr[grep("std", ftr$V2), ] #extract only values for std
+        ftr <- as.data.frame(t(ftr)) #transmute to use for column labels in mrg
+        names <- as.character(ftr[2, ])
+        colnames(mrg) <- names
+        extract <- mrg[, grep("mean", colnames(mrg))] 
+                %>% cbind(mrg[, grep("std", colnames(mrg))]) #chain to extract only "mean" and "std" variables
+
         mrg3 <- rbind(ftrsub, ftrsub2) #combine features into one df
         mrg3 <- arrange(mrg3, mrg3$V1)
-        cols_trainTest <- mrg2[, 1] #identify integer values for matching mrg columns
+        cols_trainTest <- mrg3[, 1] #identify integer values for matching mrg columns
         vals <- mrg[, cols_trainTest] #df with only values for mean and std
         y_new <- rbind(y_test, y_train)
         colnames(y_new) <- "Activity" #assign label to activity types
