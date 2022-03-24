96b2da8dc60a79ec9c93fc57fa13b50dcf12d0e8
Adam Hemingway <adam.j.hemingway@gmail.com>
2022-03-24 19:25:49 -0400

    use extract to assign activity names to first column

--- run_analysis.R
+++ run_analysis.R
@@ -6,18 +6,17 @@ run_analysis <- function(x){
         y_train <- read.table("y_train.txt")
         subtest <- read.table("subject_test.txt")
         subtrain <- read.table("subject_train.txt")
+        y_new <- rbind(y_test, y_train) #training labels and test labels
         mrg <- rbind(test, train)
         ftr <- as.data.frame(t(ftr)) #transmute to use for column labels in mrg
         names <- as.character(ftr[2, ])
         colnames(mrg) <- names
         extract <- mrg[, grep("mean", colnames(mrg))] 
                 %>% cbind(mrg[, grep("std", colnames(mrg))]) #chain to extract only "mean" and "std" variables
-
-        mrg3 <- rbind(ftrsub, ftrsub2) #combine features into one df
-        mrg3 <- arrange(mrg3, mrg3$V1)
-        cols_trainTest <- mrg3[, 1] #identify integer values for matching mrg columns
-        vals <- mrg[, cols_trainTest] #df with only values for mean and std
-        y_new <- rbind(y_test, y_train)
+        extract <- cbind(y_new, extract) #then append training labels to first column
+        
+        #CONTINUE HERE TO ASSIGN ACTIVITY NAMES IN PLACE OF NUMBERS FOR FIRST COLUMN
+        
         colnames(y_new) <- "Activity" #assign label to activity types
         namesMatch <- cbind(y_new, vals)
         desc_names <- c("walking", "walking upstairs", "walking downstairs", "sitting", "standing", "laying")
